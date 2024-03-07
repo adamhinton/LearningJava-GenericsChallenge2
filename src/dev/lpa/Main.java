a4b553c478869e6a889080188a69567d222073ce
adamd <adam.d.hinton@gmail.com>
2024-03-07 11:29:57 -0500

    working solution

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,108 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package dev.lpa;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
+
+interface Mappable{
+    void render();
+}
+
+class Point implements Mappable{
+    private double[] location;
+    private String name;
+
+    private String location(){
+        return "" + location[0] + location[1];
+    }
+
+    public Point(double[] location, String name) {
+        this.location = location;
+        this.name = name;
+    }
+
+    // Don't actually know what this does
+    public void render(){
+
+    }
+}
+
+class Park extends Point{
+    String parentService;
+
+    public Park(double[] location, String name, String parentService) {
+        super(location, name);
+        this.parentService = parentService;
+    }
+}
+
+class Line implements Mappable{
+    private double[][] location;
+    private String name;
+
+    @Override
+    public void render() {
+
+    }
+
+    public Line(double[][] location, String name) {
+        this.location = location;
+        this.name = name;
+    }
+}
+
+class River extends Line{
+    private int depth;
+
+    public River(double[][] location, String name, int depth) {
+        super(location, name);
+        this.depth = depth;
+    }
+}
+
+// Mappable interface
+    // one abst mtd: render
+
+// two classes impl Mappable:
+
+// class Point impl Mpbl
+    // two specific classes that extend this
+        // mappable item of interest
+
+// Park point, River line
+
+// class Line impl Mpbl
+    // two specific classes that extend this
+        // mappable item of interest
+
+// Generic class called Layer
+    // one type parameter: Only allow Mappable elements
+    // A single private field, a list of elements to be mapped
+    // method or constructor, or both, to add elements
+    // method renderLayer() that loops through all elements and executes    render() on each element
+
+class Layer <T extends Mappable> {
+    private List<T> elementList = new ArrayList<T>();
+
+    public List<T> getElementList() {
+        return elementList;
+    }
+
+    public void addELement(T element){
+        elementList.add(element);
+    }
+}
+
+//Main:
+    // make inst's of specific classes
+    // These get added to a typed Layer, and the renderLayer() method is called on that
+
+// Gen instc:
+// Add constr or mtds to support adding a couple adttributes, and location data, to your two specific classes
+// Loc data can be a String or set of double values
+
